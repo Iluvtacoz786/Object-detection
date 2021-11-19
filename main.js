70a48ffa5d25ae6b65c0dad170b712681426255c
Zoya Rana <74219818+Iluvtacoz786@users.noreply.github.com>
2021-11-18 17:55:24 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 var img=""
+var status=""
+object=[]
 function preload(){
-    img=loadImage("dog_cat.jpg")
+    img=loadImage("dog2cat2.jpeg")
 }
 function setup(){
 canvas=createCanvas(500,400);
 canvas.position(400,150);
+objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+document.getElementById("status").innerHTML="status: detecting object"
+
 }
 function draw(){
     image(img,0,0,500,400)
-    fill("red");
-    text("Dog",200,380);
-    noFill()
-    stroke("red")
-    rect(0,30,250,360)
+    if(status!=""){
+        for(i=0;i<object.length;i++){
+            document.getElementById("status").innerHTML="Status: Object detected"
+            fill("red");
+            text(object[i].label,object[i].x,object[i].y);
+            noFill()
+            stroke("red")
+            rect(object[i].x,object[i].y,object[i].width,object[i].height)
+        }
+    }
+   
 
-    fill("red");
-    text("Cat",400,380);
-    noFill()
-    stroke("red")
-    rect(0,30,500,360)
+   
+}
+function modelLoaded(){
+    console.log("model is loaded")
+    status=true;
+    objectDetector.detect(img,gotResult)
+}
+function gotResult(error, result){
+if(error){
+console.log(error)
+}
+else{
+    object=result;
+    console.log(result)
+}
 }
